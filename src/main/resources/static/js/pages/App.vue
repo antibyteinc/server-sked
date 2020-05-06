5886e52fa697fb4b93e9ef677b73181ced6a85e8
antibyteinc <antibyteinc@gamil.com>
2020-05-06 20:56:54 +0300

    Init

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            <button>Вхід</button>
+            <button>Реєстрація</button>
+        </div>
+        <div class="content"></div>
+        <div class="footer"></div>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style>
+
+</style>
