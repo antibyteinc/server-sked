fdc34f95ad1a8fbac8fb584bd3cba87c9b9c60ab
antibyteinc <antibyteinc@gamil.com>
2020-05-26 10:32:21 +0300

    d

--- src/main/java/server/config/WebMvcConfig.java
+++ src/main/java/server/config/WebMvcConfig.java
@@ -2,12 +2,17 @@ package server.config;
 
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -17,5 +22,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
         return container -> {
             container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/"));
         };
+    }
+
+    @Bean
+    public FilterRegistrationBean encodingFilterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        registrationBean.setFilter(new CharacterEncodingFilter());
+        Map<String, String> params = new HashMap<>();
+        params.put("encoding", "UTF-8");
+        params.put("forceEncoding", "true");
+        registrationBean.setInitParameters(params);
+        registrationBean.addUrlPatterns("/");
+        return registrationBean;
     }
 }
